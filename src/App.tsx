2f326ed09b3510a60ea6d4006c786a63291e98c5
GaganSingh5 <gagansingh0797@gmail.com>
2024-03-09 23:54:04 -0500

    Added Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
-// import "./App.css";
+import Home from "./pages/Home.component";
 
 function App() {
 
   return (
     <>
+    <Home />
     </>
   );
 }
---
+++ src/pages/Home.component.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar/Nav.component";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default Home;
