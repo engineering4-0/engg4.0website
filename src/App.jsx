1e9e69d217cccc2bd5ff18dca245f6b2bb4ac6de
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2024-05-22 22:54:40 -0400

    installed vite

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AboutPage from "./pages/About/AboutPage";
 import HomePage from "./pages/Home/HomePage";
-import AdminPage from "./pages/AdminPage/Admin.page";
 import EventsPage from "./pages/EventsPage/EventsPage";
-import ErrorPage from "./pages/ErrorPage/ErrorPage.page";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import BuddyPage from "./pages/Buddy/BuddyPage";
 import BuddyListPage from "./pages/Buddy/BuddyList";
-import UserInfo from "./pages/Temp/UserInfo.Screen";
 import MembersPage from "./pages/MembersPage";
 import Footer from "./components/Footer/Footer";
 
@@ -23,12 +21,10 @@ function App() {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/register" element={<RegistrationPage />}></Route>
         <Route path="/about" element={<AboutPage />}></Route>
-        <Route path="/admin" element={<AdminPage />}></Route>
         <Route path="/events" element={<EventsPage />}></Route>
         <Route path="/buddy" element={<BuddyPage />}></Route>
         <Route path="/buddy-list" element={<BuddyListPage />}></Route>
         <Route path="/members" element={<MembersPage />}></Route>
-        <Route path="/temp" element={<UserInfo />}></Route>
         <Route path="/*" element={<ErrorPage />}></Route>
       </Routes>
       <Footer />
