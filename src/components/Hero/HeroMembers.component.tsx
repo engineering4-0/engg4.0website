3381cdcc84fb40cbff97ba3a8e2256f388b22a76
GaganSingh5 <gagansingh0797@gmail.com>
2024-05-31 01:02:10 -0400

    Fixed color issues and refactored code

--- src/components/Hero/HeroMembers.component.tsx
+++ src/components/Hero/HeroMembers.component.tsx
@@ -82,7 +82,7 @@ const HeroMembers = () => {
           />
         );
       })}
-      <div className="text-onBackground flex aspect-square h-10 w-10 items-center justify-center rounded-full bg-primary-100 object-cover object-top p-2 text-sm">
+      <div className="text-onPrimary flex aspect-square h-10 w-10 items-center justify-center rounded-full bg-primary object-cover object-top p-2 text-sm">
         +50
       </div>
       <span className="text-onBackground ml-1.5">Volunteers</span>
--- src/components/Hero/HeroPhotoCard.component.tsx
+++ src/components/Hero/HeroPhotoCard.component.tsx
@@ -10,10 +10,10 @@ interface IHeroPhotoCard {
 const HeroPhotoCard = (props: IHeroPhotoCard) => {
   return (
     <div
-      className={`${props.className} hidden flex-col items-center justify-self-center overflow-clip rounded-lg bg-white p-2 shadow-2xl shadow-gray-600 lg:flex`}
+      className={`${props.className} bg-surface hidden flex-col items-center justify-self-center overflow-clip rounded-lg p-2 shadow-2xl shadow-onBackground lg:flex`}
     >
       <img src={props.imageURL} alt="" className="aspect-square w-full max-w-48 rounded-sm object-cover" />
-      <div className="my-1 max-w-40 text-center text-xs">{props.caption ?? "Engineering club 4.0 event"}</div>
+      <div className="my-1 max-w-40 text-center text-xs text-onBackground">{props.caption ?? "Engineering club 4.0 event"}</div>
     </div>
   );
 };
