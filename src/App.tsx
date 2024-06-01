8b447163e1bdffaeec317e4ca99bbd66c7efa5b6
GaganSingh5 <gagansingh0797@gmail.com>
2024-05-31 20:47:15 -0400

    fixed broken static asset issue and client side routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.component";
 import About from "./pages/About.page";
 import Navbar from "./components/Navbar/Navbar.component";
@@ -8,14 +8,14 @@ function App() {
 
   return (
     <div className="bg-background">
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
 
   );
