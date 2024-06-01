6cd34b2b19f2399cc86c47086640a83013cedf47
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2024-05-31 22:33:14 -0400

    Migrated registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.component";
-import About from "./pages/About.page";
-import Navbar from "./components/Navbar/Navbar.component";
-import Footer from "./components/Footer/Footer.component";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.component';
+import About from './pages/About.page';
+import Navbar from './components/Navbar/Navbar.component';
+import Footer from './components/Footer/Footer.component';
+import RegistrationPage from './pages/Registration.page';
 
 function App() {
-
   return (
     <div className="bg-background">
       <BrowserRouter>
@@ -13,11 +13,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="/register" element={<RegistrationPage />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
-
   );
 }
 
---
+++ src/pages/Registration.page.tsx
@@ -0,0 +1,7 @@
+import Registration from '../components/Registration/Registration.component';
+
+const RegistrationPage = () => {
+  return <Registration />;
+};
+
+export default RegistrationPage;
