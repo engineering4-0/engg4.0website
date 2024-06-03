1dfd8a4afd0f0038656bc2e5ea3a5637b4596880
GaganSingh5 <gagansingh0797@gmail.com>
2024-06-03 19:32:17 -0400

    added type for react-vertical-timeline

--- src/react-vertical-timeline-component.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'react-vertical-timeline-component' {
-    import { ComponentType } from 'react';
-  
-    export const VerticalTimeline: ComponentType<any>;
-    export const VerticalTimelineElement: ComponentType<any>;
-  }
-  
