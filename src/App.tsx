525969219fddc683bbeccc695ce32729b61275bf
GaganSingh5 <gagansingh0797@gmail.com>
2024-06-07 18:41:06 -0400

    firebase initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.page";
-import About from "./pages/About.page";
-import Event from "./pages/Event.page";
-import Navbar from "./components/Navbar/Navbar.component";
-import Footer from "./components/Footer/Footer.component";
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.page'
+import About from './pages/About.page'
+import Event from './pages/Event.page'
+import Navbar from './components/Navbar/Navbar.component'
+import Footer from './components/Footer/Footer.component'
 
 function App() {
-
   return (
     <div className="bg-background">
       <HashRouter>
@@ -19,8 +18,7 @@ function App() {
         <Footer />
       </HashRouter>
     </div>
-
-  );
+  )
 }
 
-export default App;
+export default App
