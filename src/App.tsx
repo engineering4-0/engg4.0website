63dc57c9e8c6bef49cd8fe4706e8f45e363007ca
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2024-06-09 19:42:34 -0400

    fixed errors caused by merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.page';
 import About from './pages/About.page';
@@ -8,23 +7,6 @@ import Footer from './components/Footer/Footer.component';
 import AdminGuard from './guards/AdminGuard.component';
 import Admin from './pages/Admin.page';
 import RegistrationPage from './pages/Registration.page';
-=======
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home.page'
-import About from './pages/About.page'
-import Event from './pages/Event.page'
-import Navbar from './components/Navbar/Navbar.component'
-import Footer from './components/Footer/Footer.component'
-<<<<<<< HEAD
->>>>>>> 5259692 (firebase initial setup)
-=======
-import AdminGuard from './guards/AdminGuard.component'
-import Admin from './pages/Admin.page'
-<<<<<<< HEAD
->>>>>>> 057c332 (added admin flow and firebase hook)
-=======
-import RegistrationPage from './pages/Registration.page'
->>>>>>> 9feda85 (Migrated registration page)
 
 function App() {
   return (
@@ -38,23 +20,12 @@ function App() {
           <Route path="/admin" element={<AdminGuard />}>
             <Route path="" element={<Admin />}></Route>
           </Route>
-<<<<<<< HEAD
-<<<<<<< HEAD
           <Route path="/register" element={<RegistrationPage />}></Route>
-=======
->>>>>>> 057c332 (added admin flow and firebase hook)
-=======
-          <Route path="/register" element={<RegistrationPage />}></Route>
->>>>>>> 9feda85 (Migrated registration page)
         </Routes>
         <Footer />
       </HashRouter>
     </div>
-<<<<<<< HEAD
   );
-=======
-  )
->>>>>>> 5259692 (firebase initial setup)
 }
 
 export default App
